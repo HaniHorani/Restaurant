10ed33cefe7e25adc8afeb566cf55675f38b7e49
shadow55899 <kidstan2@gmail.com>
2024-12-16 11:22:22 +0300

    new frame

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 package src;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setTitle("new frame");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
     }
 }
